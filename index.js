6466de4189d459b07e81d733c059983bdfc9f6d0
AngryCashews234 <33270165+AngryCashews234@users.noreply.github.com>
2017-11-07 19:09:16 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var obj = JSON.parse ('{ "fname":"rafael","lname":"marques","age":"19"}');
+var obj2 = JSON.parse ('{ "fname":"daniel","lname":"marques","age":"19"}');
+console.log(obj);
+console.log(obj2);
